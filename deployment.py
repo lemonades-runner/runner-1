7da3d2959247932b11d1b7345ca71392b0aeb906
Aleksei Orlov <alexeyorlov65@gmail.com>
2024-04-12 21:19:50 +0300

    deployment module logs

--- deployment.py
+++ deployment.py
@@ -9,7 +9,10 @@ if resp.status_code == 200:
     upstream_name = str(uuid4()).replace('-', '').strip()[:6]
     upstream_url = f'https://{upstream_name}.share.zrok.io'
     with open('.env', 'w', encoding='utf-8') as f:
-        f.write(f"DEPLOYMENT_UUID={deployment['uuid']}\n"
+        envs = (f"DEPLOYMENT_UUID={deployment['uuid']}\n"
                 f"DEPLOYMENT_IMAGE={deployment['image']}\n"
                 f"UPSTREAM_NAME={upstream_name}\n"
                 f"UPSTREAM_URL={upstream_url}\n")
+        print('Successfully fetched envs:')
+        print(envs)
+        f.write(envs)
