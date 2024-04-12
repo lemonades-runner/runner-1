68c9e7db64aa52561a752f298c19a5bf64344e8e
Aleksei Orlov <alexeyorlov65@gmail.com>
2024-04-12 23:49:56 +0300

    python scripts to handle the deployment

--- before.py
+++ before.py
@@ -3,10 +3,10 @@ from subprocess import run
 from deployment import prepare_deployment
 
 ZROK_TOKEN = os.getenv('ZROK_TOKEN')
-UPSTREAM_NAME = os.getenv('UPSTREAM_NAME')
 
 # Prepare deployment
 prepare_deployment()
+UPSTREAM_NAME = os.getenv('UPSTREAM_NAME')
 
 # Enable zrok environment
 run(['zrok', 'enable', ZROK_TOKEN, '-d', UPSTREAM_NAME])
